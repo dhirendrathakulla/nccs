a7003e7d4fb8992c9098c8fb2f4e04094c9f470a
dhirendra <dhiren.thakulla@gmail.com>
2023-02-13 08:53:40 +0545

    node basic

---
+++ common/validationHelper.js
@@ -0,0 +1,7 @@
+((validationHelper) =>{
+    validationHelper.validEmail = async (email) => {
+        let isValidEmail = false;
+        // TODO logic for email
+        return isValidEmail
+    }   
+})()
---
+++ routes/user/methods/signup.js
@@ -0,0 +1,7 @@
+(() => {
+    // const helper = require("./../../../common/index")
+    module.exports = async(req, res, next) => {
+
+        console.log(req.body);
+    }
+})()
